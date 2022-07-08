b085fc7618d9ec82cc26ff59dbcdb050dc0e4f3d
Akankshakrishna <akanksha36200@gmail.com>
2022-07-08 15:01:23 +0530

    Details displayed in other view controller.

--- API/Controllers/DetailsViewController.swift
+++ API/Controllers/DetailsViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var profile_pic: UIImageView!
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var bday: UILabel!
+    @IBOutlet weak var status: UILabel!
+    @IBOutlet weak var nickname: UILabel!
+    
+    var name_: String = "",bday_ : String = "",occu_: [String] = [],status_ : String = "",nickname_: String = "", appearance_: [Int] = [],portrayed_ : String = "",category_: String = "", im_ = UIImage(), id_: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        profile_pic.image = im_
+        id.text = "ID: \(id_)"
+        name.text = "Name: \(name_)"
+        bday.text = "Birthday: \(bday_)"
+        status.text = "Status: \(status_)"
+        nickname.text = "NickName: \(nickname_)"
     }
 
 
--- API/Controllers/ViewController.swift
+++ API/Controllers/ViewController.swift
@@ -9,10 +9,10 @@ import UIKit
 
 class ViewController: UIViewController, DataManagerDelegate {
     
-//    var name: String = "",bday : String = "",occu: [String] = [],status : String = "",nickname: String = "", appearance: [Int] = [],portrayed : String = "",category: String = "", im = UIImage(), id: Int = 0
     var names : [String] = []
     var searchNames : [String] = []
     var searchActive : Bool = false
+    var final_data :[DataModel] = []
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var namesTable: UITableView!
@@ -28,15 +28,34 @@ class ViewController: UIViewController, DataManagerDelegate {
     
     
     func didUpdateData(_ dataManager: DataManager, dataa: [DataModel]) {
-//        name = dataa.name; id = dataa.id; bday = dataa.bday; occu = dataa.occu; status = dataa.status; nickname = dataa.nickname; appearance = dataa.appearance; portrayed = dataa.portrayed; category = dataa.category; im = dataa.im
         
         for i in 0...61{
             names.append(dataa[i].name)
         }
+        
+        final_data = dataa
     
         DispatchQueue.main.async {
             self.namesTable.reloadData()
         }
+    }
+    
+    func getDetails(dataa: [DataModel], i: Int) -> DataModel {
+        
+        let id = dataa[i].id
+        let name = dataa[i].name
+        let bday = dataa[i].bday
+        let occu = dataa[i].occu
+        let status  = dataa[i].status
+        let nickname = dataa[i].nickname
+        let appearance = dataa[i].appearance
+        let portrayed = dataa[i].portrayed
+        let category = dataa[i].category
+        let im = dataa[i].im
+        
+        let data_required = DataModel(id: id, name: name, bday: bday, occu: occu, im: im, status: status, nickname: nickname, appearance: appearance, portrayed: portrayed, category: category)
+                    
+        return data_required
     }
 
 }
@@ -60,6 +79,20 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
             cell.textLabel?.text = "\(indexPath.row + 1). \(names[indexPath.row])"
         }
         return cell
+    }
+
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "gotoDetails", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! DetailsViewController
+        
+        if let indexPath = namesTable.indexPathForSelectedRow{
+            let r_data = getDetails(dataa: final_data, i: indexPath.row)
+            vc.name_ = r_data.name; vc.id_ = r_data.id; vc.bday_ = r_data.bday; vc.status_ = r_data.status; vc.nickname_ = r_data.nickname; vc.im_ = r_data.im
+        }
     }
     
 }
