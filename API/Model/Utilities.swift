b987214965be5244dfa08c528a2c9031128a8ac7
Akankshakrishna <akanksha36200@gmail.com>
2022-07-11 13:57:27 +0530

    Modified some UI properties.

--- API/Model/Utilities.swift
+++ API/Model/Utilities.swift
@@ -13,3 +13,34 @@ class Utilities{
     var names : [String] = [], finalData : [String] = [], images : [UIImage] = [], bdays : [String] = [], nickNames : [String] = [], statuses : [String] = [], id : [Int] = [], final_data :[DataModel] = [], pq = Int()
     
 }
+
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
